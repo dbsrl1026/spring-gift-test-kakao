f1c97d6e1f3e769e7a28c0026fec5afe204ad416
danny.197 <hyg4work@gmail.com>
2026-02-24 10:13:21 +0900

    Configuration 구현

---
+++ src/test/java/gift/cucumber/CucumberSpringConfiguration.java
@@ -0,0 +1,21 @@
+package gift.cucumber;
+
+import io.cucumber.spring.CucumberContextConfiguration;
+import io.restassured.RestAssured;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
+
+@CucumberContextConfiguration
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("cucumber")
+public class CucumberSpringConfiguration {
+
+    @LocalServerPort
+    private int port;
+
+    @jakarta.annotation.PostConstruct
+    public void setUp() {
+        RestAssured.port = port;
+    }
+}
